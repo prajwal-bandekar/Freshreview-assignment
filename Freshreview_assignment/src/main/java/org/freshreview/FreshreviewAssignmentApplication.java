998c4fbc3062ca045ba884f07a6b924b6b4cccf0
Prajwal <prajwalbandekar@gmail.com>
2023-11-02 23:18:51 +0530

    Building the project

---
+++ Freshreview_assignment/src/main/java/org/freshreview/FreshreviewAssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.freshreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshreviewAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshreviewAssignmentApplication.class, args);
+	}
+
+}
